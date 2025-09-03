0f2a186823c7d530bc5629ee67f8d7de6c96d4d2
Brian Phan <brianphan@Brians-MacBook-Pro.local>
2025-09-03 16:12:06 +1000

    Setting up docker and typescript

---
+++ backend/prisma/migrations/20250902092017_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "public"."users" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "public"."users"("email");
